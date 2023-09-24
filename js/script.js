c15970d2a0a8432674c03c46c5d2d3f238702ed6
yazshrilld <yazshrilld@gmail.com>
2023-09-24 16:57:48 +0100

    code update

--- js/script.js
+++ js/script.js
@@ -25,28 +25,7 @@ const cardCvc_1 = cardCvc.parentElement;
 let cardName_11 = "";
 // console.log("Card Number: ", cardCvc.parentElement);
 // console.log("Card Number: ", cardCvc.previousElementSibling);
-let strLength;
-console.log({strLength});
-const input = document.querySelector(".input-text");
-input.addEventListener("input", (e) => {
-  let string = e.target.value;
-  console.log("From Initial: ", { string });
-  string = string.replace(/\s/g, "");
-  console.log("From Replace: ", { string });
 
-  let resultString = "";
-  for (let i = 0; i < string.length; i++) {
-    resultString += string[i];
-    console.log("from inside: ", { resultString });
-    console.log("from length: ", resultString.length);
-    if ((i + 1) % 4 === 0 && i !== string.length - 1) {
-      console.log("From i: ", i);
-      resultString += " ";
-    }
-  }
-  input.value = resultString;
-  strLength = resultString
-});
 function nameSuccess(input, message) {
   const errorControl = input.parentElement;
   errorControl.className = "form--name success";
@@ -142,21 +121,18 @@ function checkInputs() {
   }
 
   if (cardNumberValue === "") {
-    console.log("Reault String: ", resultString.length);
     numError(cardNumber, "enter your card number naaaaaaaa!!!!😠😠😠😠");
-  }   
-  else if (cardNumberValue.length <= 18) {
+  } else if (cardNumberValue.length <= 15) {
     numError(
       cardNumber,
       "why you de enter less than 16 digit number!!😠😠😠😠 Take time ooo!!!"
     );
-  } else if (cardNumberValue.length > 19) {
+  } else if (cardNumberValue.length > 16) {
     numError(
       cardNumber,
       "It's more than 16 charrecters, apologise before you proceed😠😠!!!"
     );
-  }
-  else {
+  } else {
     numSuccess(cardNumber);
   }
 
@@ -240,8 +216,8 @@ form.addEventListener("submit", (e) => {
       console.log("Yazid");
       thanksCard.classList.add("success");
       cardsForm.classList.add("completed");
-    }
-  }
+    } 
+  } 
 
   checkInputs();
 });
